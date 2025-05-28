68d1c816eeeb1df28760f41885b15178651a4b85
Farhad Sultanov <sultanovv991@gmail.com>
2025-05-28 14:31:58 +0400

    Update tikbot.py

--- tikbot.py
+++ tikbot.py
@@ -15,14 +15,16 @@ def index():
 def download():
     video_url = request.form['url']
     format_type = request.form['format']
-    file_ext = 'mp4' if format_type == 'mp4' else 'm4a'
+    file_ext = 'mp4' if format_type == 'mp4' else 'mp3'
     filename = f"{uuid.uuid4()}.{file_ext}"
 
     ydl_opts = {
-        'format': 'best[ext=mp4][height<=360]' if format_type == 'mp4' else 'bestaudio[ext=m4a]/bestaudio',
         'outtmpl': filename,
-        'cookiefile': 'cookies.txt',  # ✅ cookie faylını tanıtdıq
-        'quiet': True
+        'quiet': True,
+        'format': 'bv*[ext=mp4][height<=360]+ba[ext=m4a]/b[ext=mp4]' if format_type == 'mp4' else 'bestaudio[ext=m4a]/bestaudio',
+        'merge_output_format': None,  # ffmpeg olmasın
+        'noplaylist': True,
+        'cookiefile': 'cookies.txt',
     }
 
     def delayed_remove(path):
@@ -30,8 +32,11 @@ def download():
         if os.path.exists(path):
             os.remove(path)
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([video_url])
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([video_url])
+    except Exception as e:
+        return f"Xəta baş verdi: {e}"
 
     threading.Thread(target=delayed_remove, args=(filename,)).start()
     return send_file(filename, as_attachment=True)
